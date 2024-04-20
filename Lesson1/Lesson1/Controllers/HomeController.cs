89595832bd3eeead5a93fc34823c845bf0a3174b
Truong_Anh <anhnganh04@gmail.com>
2024-04-20 22:04:26 +0700

    Lesson1

---
+++ Lesson1/Lesson1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.message = "Chào Mừng bạn đến với ASP.NET MVC 5";
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.message = "Đây là trang About";
+            return View();
+        }
+    }
+
+}
---
+++ Lesson1/bai 1.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bai_1._1.MvcApplication" Language="C#" %>
